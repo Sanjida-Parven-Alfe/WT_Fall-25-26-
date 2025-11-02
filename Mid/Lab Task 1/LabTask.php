a9d52af5cc55650507bff8a99bbf728bd8223fe1
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-11-02 20:42:28 +0600

    first commit

---
+++ Mid/Lab Task 1/LabTask.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Blood Management System</title>
+</head>
+
+<body>
+    <h1>Blood Donation Form</h1>
+    <br> <br> <br> <br>
+
+    Name:
+    <input type="text"> <br> <br> <br>
+    Age:
+    <input type="number"> <br> <br> <br>
+    Email: 
+    <input type="email"> <br> <br> <br>
+    Blood Group: 
+    <input type="radio" >A+
+    <input type="radio" >A-
+    <input type="radio" >B+
+    <input type="radio" >B-
+    <input type="radio" >O+
+    <input type="radio" >O-
+
+    <br> <br> <br>
+
+    Have you any previous blood donate history:
+    <input type="checkbox">Yes
+    <input type="checkbox">No
+
+    <br><br><br>
+    <input type="submit">
+</body>
+</html>
---
+++ Mid/Lab Task 1/lab1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+
+    </title>
+</head>
+
+<body>
+    <center>
+        <h1>My name is Alfe</h1>
+    </center>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem unde sit sint explicabo nobis quis labore, rem, eos dicta iure perferendis asperiores sunt et dolore deleniti? Fugit quidem eos iure.</p>
+
+    <br> <br> <br> 
+
+    Enter Your Name:   
+    <input type="text">
+    <br> <br>
+    Enter Your Age:  
+    <input type="number">
+    <br> <br>
+    Enter Your DOB:  
+    <input type="date">
+    <br> <br>
+    Language: 
+    <input type="radio">Bangla
+    <input type="radio" name="" id="">English
+
+    <br> <br>
+
+    Semester: 
+    <input type="checkbox"> 1st
+    <input type="checkbox"> 2nd
+
+    <br> <br>
+
+    <input type="submit">
+
+</body>
+
+</html>
