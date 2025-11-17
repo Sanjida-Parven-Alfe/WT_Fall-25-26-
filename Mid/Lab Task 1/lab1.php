9c5c392089bef89c71d8475b241ace916fa49a86
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-11-17 11:07:41 +0600

    First commit

--- Mid/Lab Task 1/lab1.php
+++ Mid/Lab Task 1/lab1.php
@@ -1,42 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html >
 <head>
-    <title>
+    <title>I LOve you</title>
 
-    </title>
+    <style>
+        p{
+            border: 5px;
+            border-style: dashed;
+            border-color: blue;
+        }
+    </style>
 </head>
-
 <body>
-    <center>
-        <h1>My name is Alfe</h1>
-    </center>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem unde sit sint explicabo nobis quis labore, rem, eos dicta iure perferendis asperiores sunt et dolore deleniti? Fugit quidem eos iure.</p>
-
-    <br> <br> <br> 
-
-    Enter Your Name:   
-    <input type="text">
-    <br> <br>
-    Enter Your Age:  
-    <input type="number">
-    <br> <br>
-    Enter Your DOB:  
-    <input type="date">
-    <br> <br>
-    Language: 
-    <input type="radio">Bangla
-    <input type="radio" name="" id="">English
-
-    <br> <br>
-
-    Semester: 
-    <input type="checkbox"> 1st
-    <input type="checkbox"> 2nd
-
-    <br> <br>
-
-    <input type="submit">
-
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga expedita eligendi asperiores facere eaque tempora placeat omnis obcaecati, minima, unde nemo odit assumenda repellat facilis, quos modi eveniet enim dicta.</p>
 </body>
-
 </html>
