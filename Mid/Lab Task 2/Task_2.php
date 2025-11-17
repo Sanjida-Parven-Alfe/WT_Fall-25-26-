9f77770e65829c8c2b741a0e805df7fbb5d7dd13
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-11-17 12:10:07 +0600

    8th

--- Mid/Lab Task 2/Task_2.php
+++ Mid/Lab Task 2/Task_2.php
@@ -7,25 +7,40 @@
     <h1>Student Registration</h1>
 
     <p>Full Name:</p>
-    <input type="text">
+    <input type="text" id="fullName">
 
     <p>Email:</p>
-    <input type="email">
+    <input type="email" id="email">
 
     <p>Password:</p>
-    <input type="password">
+    <input type="password" id="password">
 
     <p>Confirm Password:</p>
-    <input type="password">
+    <input type="password" id="confirmPass">
 
-   <p><input type="submit" value="Register"></p> 
+    <p><input type="submit" value="Register"></p> 
+
+    <div id="studentError">  </div>
+    <div id="studentOutput"> </div>
 
     <h1>Course Registration</h1>
 
     <p>Course Name:</p>
-    <input type="text">
+    <input type="text" id="courseName">
     <p> <input type="submit" value="Add Course"> </p>
 
-    
+    <div id="courseError"></div>
+    <div id="courseOutput"></div>
+
+    <script>
+        // Student Validation
+        function studentRegister () {
+            let name = document.getElementById("fullName").value.trim();
+            let email = document.getElementById("email").value.trim();
+            let pass = document.getElementById("password").value.trim();
+            let cpass = document.getElementById("confirmPass").value.trim();
+        }
+
+    </script>
 </body>
 </html>
