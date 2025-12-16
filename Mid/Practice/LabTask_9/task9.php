be2117b89aa59d6933bce9b38e1520814dc776b5
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-12-16 22:20:54 +0600

    .

--- Mid/Practice/LabTask_9/task9.php
+++ Mid/Practice/LabTask_9/task9.php
@@ -83,7 +83,7 @@
             for (var i = 0; i < sections.length; i++) {
                 sections[i].style.display = "none";
             }
-            // Show selected section
+          
             document.getElementById(sectionId).style.display = "block";
         }
 
