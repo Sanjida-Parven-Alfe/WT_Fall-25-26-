56ca661f3a660345c89e62d56346c74db5631943
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-12-22 23:47:30 +0600

    .

--- Final/Task_2/Session/index.php
+++ Final/Task_2/Session/index.php
@@ -4,23 +4,26 @@ session_start();
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Home Page</title>
 </head>
+
 <body>
 
-<h1>Welcome to My Website</h1>
+    <h1>Welcome to My Website</h1>
 
-<?php
-if (isset($_SESSION["username"])) {
-    echo "<p>You are logged in as <b>" . $_SESSION["username"] . "</b></p>";
-    echo "<a href='dashboard.php'>Go to Dashboard</a><br><br>";
-    echo "<a href='logout.php'>Logout</a>";
-} else {
-    echo "<p>You are not logged in</p>";
-    echo "<a href='login.php'>Login</a>";
-}
-?>
+    <?php
+    if (isset($_SESSION["username"])) {
+        echo "<p>You are logged in as <b>" . $_SESSION["username"] . "</b></p>";
+        echo "<a href='dashboard.php'>Go to Dashboard</a><br><br>";
+        echo "<a href='logout.php'>Logout</a>";
+    } else {
+        echo "<p>You are not logged in</p>";
+        echo "<a href='login.php'>Login</a>";
+    }
+    ?>
 
 </body>
+
 </html>
