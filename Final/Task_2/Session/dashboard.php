49d17bf21401318370c25e0fe1b0bcf534437127
Sanjida Parven Alfe <sanjidaparvinalfe@gmail.com>
2025-12-22 23:48:01 +0600

    .

--- Final/Task_2/Session/dashboard.php
+++ Final/Task_2/Session/dashboard.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 
-if (!isset($_SESSION["username"]))
-{
+if (!isset($_SESSION["username"])) {
     header("Location:login.php");
     exit();
 }
@@ -12,19 +11,22 @@ if (!isset($_SESSION["username"]))
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Dashboard</title>
 </head>
+
 <body>
 
-<h2>Dashboard</h2>
+    <h2>Dashboard</h2>
 
-<p>Welcome, <b><?php echo $_SESSION["username"]; ?></b></p>
+    <p>Welcome, <b><?php echo $_SESSION["username"]; ?></b></p>
 
-<p>This page is only for logged-in users.</p>
+    <p>This page is only for logged-in users.</p>
 
-<a href="index.php">Home</a><br><br>
-<a href="logout.php">Logout</a>
+    <a href="index.php">Home</a><br><br>
+    <a href="logout.php">Logout</a>
 
 </body>
+
 </html>
