e4a287c0f543c789a7321075beec64de007709a6
Abrar-Fiaz <abrarfiaz10@gmail.com>
2026-01-04 20:33:09 +0600

    register php

--- BariForce/User/php/register.php
+++ BariForce/User/php/register.php
@@ -1,8 +1,28 @@
+<?php
+session_start();
+ 
+$error = "";
+$success = "";
+ 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fullname = $_POST['fullname'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirm_pass = $_POST['confirm_password'];
+ 
+    if ($password !== $confirm_pass) {
+        $error = "Passwords do not match!";
+    } else {
+        $success = "Registration Successful! You can now login.";
+    }
+}
+?>
+ 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
     <title>Create Account - BariForce</title>
     
     <link rel="stylesheet" href="../CSS/navbar.css">
@@ -11,8 +31,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
+ 
     <?php include 'navbar.php'; ?>
-
+ 
     <div class="register-container">
         <div class="register-box">
             <h2>Create Account</h2>
@@ -23,37 +44,47 @@
                     <i class="fas fa-id-card"></i>
                     <input type="text" name="fullname" placeholder="Full Name" required>
                 </div>
-
+ 
                 <div class="input-group">
                     <i class="fas fa-user"></i>
                     <input type="text" name="username" placeholder="Username" required>
                 </div>
-
+ 
                 <div class="input-group">
                     <i class="fas fa-envelope"></i>
                     <input type="email" name="email" placeholder="Email Address" required>
                 </div>
-
+ 
                 <div class="input-group">
                     <i class="fas fa-lock"></i>
                     <input type="password" name="password" placeholder="Password" required>
                 </div>
-
+ 
                 <div class="input-group">
                     <i class="fas fa-check-circle"></i>
                     <input type="password" name="confirm_password" placeholder="Confirm Password" required>
                 </div>
-
+ 
+                <?php if($error): ?>
+                    <p class="error-msg"><?php echo $error; ?></p>
+                <?php endif; ?>
+ 
+                <?php if($success): ?>
+                    <p class="success-msg"><?php echo $success; ?> <a href="../../Admin/php/login.php">Login here</a></p>
+                <?php endif; ?>
+ 
                 <button type="submit" class="register-btn">Sign Up</button>
             </form>
-
+ 
             <div class="links">
                 <span>Already have an account? <a href="../../Admin/php/login.php" class="login-link">Login</a></span>
             </div>
         </div>
     </div>
-
+ 
     <?php include 'footer.php'; ?>
+ 
     <script src="../js/themeToggle.js"></script>
+ 
 </body>
 </html>
