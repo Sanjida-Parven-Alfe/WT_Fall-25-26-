8761a03a84943d987911737042ae75b95657ffe7
Abrar-Fiaz <abrarfiaz10@gmail.com>
2026-01-04 20:23:48 +0600

    register php

---
+++ BariForce/User/php/register.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Create Account - BariForce</title>
+
+    <link rel="stylesheet" href="../CSS/navbar.css">
+    <link rel="stylesheet" href="../CSS/register.css">
+    <link rel="stylesheet" href="../CSS/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+
+    <div class="register-container">
+        <div class="register-box">
+            <h2>Create Account</h2>
+            <p>Join BariForce community today</p>
+        </div>
+    </div>
+
+    <?php include 'footer.php'; ?>
+    <script src="../js/themeToggle.js"></script>
+</body>
+
+</html>
