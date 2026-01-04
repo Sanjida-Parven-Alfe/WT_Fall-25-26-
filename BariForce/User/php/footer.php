6d038b91c1dda4144a94297c873730ed4a81e817
Abrar-Fiaz <abrarfiaz10@gmail.com>
2026-01-04 19:59:00 +0600

    footer php

---
+++ BariForce/User/php/footer.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+ 
+<body>
+    <footer>
+        <div class="footer-container">
+            <div class="footer-col">
+                <h3>BariForce.</h3>
+                <p>Your trusted home service partner. Verified professionals at your doorstep.</p>
+            </div>
+            <div class="footer-col">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="services.php">Services</a></li>
+                    <li><a href="../../Admin/php/login.php">Login</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h3>Contact</h3>
+                <ul>
+                    <li><i class="fas fa-phone"></i> +880 1234 567 890</li>
+                    <li><i class="fas fa-envelope"></i> support@bariforce.com</li>
+                </ul>
+            </div>
+        </div>
+        <div class="copyright">
+            <p>&copy; 2026 BariForce System. All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+ 
+</html>
