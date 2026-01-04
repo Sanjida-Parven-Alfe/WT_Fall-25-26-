e45a0bce03f4bd1870531fc05d3cf65d126e4a9e
Abrar-Fiaz <abrarfiaz10@gmail.com>
2026-01-04 18:45:38 +0600

    skeleton commit

---
+++ BariForce/User/php/home.php
@@ -0,0 +1,27 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>BariForce - Expert Home Services</title>
+
+    <link rel="stylesheet" href="../CSS/navbar.css">
+    <link rel="stylesheet" href="../CSS/home.css">
+    <link rel="stylesheet" href="../CSS/footer.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+
+<body>
+
+    <?php include 'navbar.php'; ?>
+
+
+
+    <?php include 'footer.php'; ?>
+
+    <script src="../js/themeToggle.js"></script>
+
+</body>
+
+</html>
