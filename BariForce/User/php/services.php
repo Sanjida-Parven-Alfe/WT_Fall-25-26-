74005aee6e7a6d7f88ebab461f34ed4cbc8a5b2d
Abrar-Fiaz <abrarfiaz10@gmail.com>
2026-01-14 00:51:57 +0600

    Services commit

---
+++ BariForce/User/php/services.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>All Services - BariForce</title>
+
+    <link rel="stylesheet" href="../CSS/navbar.css">
+    <link rel="stylesheet" href="../CSS/services.css">
+    <link rel="stylesheet" href="../CSS/footer.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+
+<body>
+    <?php include 'navbar.php'; ?>
+</body>
+
+</html>
