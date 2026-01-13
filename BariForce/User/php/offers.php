faaa09eb1a3888adf4b2930c728f45deef2e4092
Abrar-Fiaz <abrarfiaz10@gmail.com>
2026-01-14 02:00:23 +0600

    offers commit

---
+++ BariForce/User/php/offers.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Exclusive Offers - BariForce</title>
+
+    <link rel="stylesheet" href="../CSS/navbar.css">
+    <link rel="stylesheet" href="../CSS/offers.css">
+    <link rel="stylesheet" href="../CSS/footer.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+
+<body>
+
+    <?php include 'navbar.php'; ?>
+
+</body>
+
+</html>
