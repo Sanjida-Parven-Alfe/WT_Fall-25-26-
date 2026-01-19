ad4c4ebb596cd5c8b117dbd6c04354430bdd6f73
Sanjida-Parven-Alfe <sanjidaparvinalfe@gmail.com>
2026-01-20 01:10:09 +0600

    massage view commit

--- BariForce/Admin/html/messages-view.php
+++ BariForce/Admin/html/messages-view.php
@@ -31,7 +31,55 @@
                 <?php endif; ?>
             <?php endif; ?>
 
-            
+            <div class="message-container">
+                <table class="message-table">
+                    <thead>
+                        <tr>
+                            <th>User Info</th>
+                            <th>Subject</th>
+                            <th>Message</th>
+                            <th>Date</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if ($result->num_rows > 0): ?>
+                            <?php while($row = $result->fetch_assoc()): ?>
+                                <tr>
+                                    <td class="user-info">
+                                        <strong><?php echo htmlspecialchars($row['fullname']); ?></strong><br>
+                                        <small><?php echo htmlspecialchars($row['email']); ?></small><br>
+                                        <small><?php echo htmlspecialchars($row['phone']); ?></small>
+                                    </td>
+                                    <td>
+                                        <span class="badge-subject"><?php echo htmlspecialchars($row['subject']); ?></span>
+                                    </td>
+                                    <td class="msg-text">
+                                        <?php echo nl2br(htmlspecialchars($row['message'])); ?>
+                                    </td>
+                                    <td><?php echo date('d M, Y', strtotime($row['created_at'])); ?></td>
+                                    <td>
+                                        <a href="mailto:<?php echo $row['email']; ?>" class="btn-reply" title="Reply">
+                                            <i class="fas fa-reply"></i>
+                                        </a>
+                                        <a href="messages.php?delete_id=<?php echo $row['id']; ?>" class="btn-delete" 
+                                           onclick="return confirm('Are you sure you want to delete this message?');">
+                                            <i class="fas fa-trash-alt"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            <?php endwhile; ?>
+                        <?php else: ?>
+                            <tr>
+                                <td colspan="5" class="empty-state">
+                                    <i class="fas fa-envelope-open" style="font-size: 3rem; color: #ccc;"></i>
+                                    <p>No messages found from users.</p>
+                                </td>
+                            </tr>
+                        <?php endif; ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </section>
 
